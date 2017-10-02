27c593b6aad6ddde3612251086d3699d1c93b296
Lucas Lima <lucaslimapoa2@gmail.com>
2017-10-02 17:23:17 -0300

    Rounding decimal part. Adding more Unit Test cases.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -22,6 +22,26 @@ class Tests: XCTestCase {
         verifyFormattedStringFromDouble(value: -0.01, expectedString: "R$ -0,01")
         verifyFormattedStringFromDouble(value: -1850.01, expectedString: "R$ -1.850,01")
         verifyFormattedStringFromDouble(value: 1850.01, expectedString: "R$ 1.850,01")
+        
+        verifyFormattedStringFromDouble(value: 4.01, expectedString: "R$ 4,01")
+        verifyFormattedStringFromDouble(value: 4.02, expectedString: "R$ 4,02")
+        verifyFormattedStringFromDouble(value: 4.03, expectedString: "R$ 4,03")
+        verifyFormattedStringFromDouble(value: 4.04, expectedString: "R$ 4,04")
+        verifyFormattedStringFromDouble(value: 4.05, expectedString: "R$ 4,05")
+        verifyFormattedStringFromDouble(value: 4.16, expectedString: "R$ 4,16")
+        verifyFormattedStringFromDouble(value: 4.06, expectedString: "R$ 4,06")
+        verifyFormattedStringFromDouble(value: 6.67, expectedString: "R$ 6,67")
+        verifyFormattedStringFromDouble(value: 9.87, expectedString: "R$ 9,87")
+        
+        verifyFormattedStringFromDouble(value: 12.67, expectedString: "R$ 12,67")
+        verifyFormattedStringFromDouble(value: 65.69, expectedString: "R$ 65,69")
+        verifyFormattedStringFromDouble(value: 70.07, expectedString: "R$ 70,07")
+        verifyFormattedStringFromDouble(value: 75.08, expectedString: "R$ 75,08")
+        verifyFormattedStringFromDouble(value: 100.86, expectedString: "R$ 100,86")
+        
+        verifyFormattedStringFromDouble(value: 1000.69, expectedString: "R$ 1.000,69")
+        verifyFormattedStringFromDouble(value: 1000.98, expectedString: "R$ 1.000,98")
+        verifyFormattedStringFromDouble(value: 1000.54, expectedString: "R$ 1.000,54")
     }
     
     func test_AttributedString_ToDouble() {
@@ -29,6 +49,10 @@ class Tests: XCTestCase {
         verifyDoubleFromFormattedString(formattedString: "R$ -0,01", expectedValue: -0.01)
         verifyDoubleFromFormattedString(formattedString: "R$ -1.850,01", expectedValue: -1850.01)
         verifyDoubleFromFormattedString(formattedString: "R$ 1.850,01", expectedValue: 1850.01)
+        verifyDoubleFromFormattedString(formattedString: "R$ 4,06", expectedValue: 4.06)
+        verifyDoubleFromFormattedString(formattedString: "4,06", expectedValue: 4.06)
+        verifyDoubleFromFormattedString(formattedString: "4.06", expectedValue: 4.06)
+        verifyDoubleFromFormattedString(formattedString: String(4.02), expectedValue: 4.02)
     }
     
     func test_NonNumbersString() {
